c753653466028621fd88ef3fdc86fd9b90e3637e
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2022-05-04 20:48:02 +0200

    NEW: main elrendezes

---
+++ src/app/api-route/api-route/api-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-route',
+  templateUrl: './api-route.component.html',
+  styleUrls: ['./api-route.component.css']
+})
+export class ApiRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { FeedbackComponent } from './feedback/feedback/feedback.component';
+import { ContentComponent } from './content/content/content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiRouteComponent } from './api-route/api-route/api-route.component';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FeedbackComponent,
+    ContentComponent,
+    ApiRouteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
