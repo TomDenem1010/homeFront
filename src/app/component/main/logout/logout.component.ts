a55661758888ea3ae34def126863d92558ae34d3
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2022-05-24 19:29:39 +0200

    NEW: logout logic

--- src/app/component/main/logout/logout.component.ts
+++ src/app/component/main/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApicallService } from 'src/app/service/apicall.service';
 import { CommonService } from 'src/app/service/common.service';
 
 @Component({
@@ -17,7 +18,8 @@ export class LogoutComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private common: CommonService) { 
+    private common: CommonService,
+    private apiCall: ApicallService) { 
       this.common.apiEvent.subscribe({
         next: () => {
           this.initAutoLogout();
@@ -68,8 +70,17 @@ export class LogoutComponent implements OnInit {
   }
 
   logout() : void {
-    localStorage.clear();
-    this.common.logout();
-    this.router.navigate([""]);
+    this.apiCall.postApiCall('logout', this.getUserData()).subscribe((response: any) => {
+      localStorage.clear();
+      this.common.logout();
+      this.router.navigate([""]);
+    });
   } 
+
+  getUserData() : any {
+    return {
+      name: localStorage.getItem('name'),
+      password: localStorage.getItem('password')
+    }
+  }
 }
